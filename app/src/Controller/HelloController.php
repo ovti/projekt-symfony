bc2bb800e0579c5b3014fb423d96806f916f3367
ovti <svdovti@gmail.com>
2023-03-29 12:30:46 +0200

    test

--- app/src/Controller/HelloController.php
+++ app/src/Controller/HelloController.php
@@ -6,6 +6,7 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * Class HelloController.
@@ -17,6 +18,7 @@ class HelloController
      *
      * @return Response HTTP response
      */
+    #[Route('/hello', name: 'hello_index', methods: 'GET')]
     public function index(): Response
     {
         return new Response('Hello World!');

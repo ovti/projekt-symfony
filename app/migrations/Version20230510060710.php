ac7d825ab3823cc9162ac73aa74f1a2c0c67448e
ovti <svdovti@gmail.com>
2023-05-10 08:49:21 +0200

    php

--- app/migrations/Version20230510060710.php
+++ app/migrations/Version20230510060710.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230420181822 extends AbstractMigration
+final class Version20230510060710 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- app/src/Controller/TaskController.php
+++ app/src/Controller/TaskController.php
@@ -6,10 +6,10 @@
 namespace App\Controller;
 
 use App\Entity\Task;
-use Symfony\Component\HttpFoundation\Request;
-use Knp\Component\Pager\PaginatorInterface;
-use App\Repository\TaskRepository;
+use App\Service\TaskService;
+use App\Service\TaskServiceInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,33 +19,40 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/task')]
 class TaskController extends AbstractController
 {
-// ...
+    /**
+     * Task service.
+     */
+    private TaskServiceInterface $taskService;
+
+    /**
+     * Constructor.
+     */
+    public function __construct(TaskServiceInterface $taskService)
+    {
+        $this->taskService = $taskService;
+    }
+
     /**
      * Index action.
      *
-     * @param Request            $request        HTTP Request
-     * @param TaskRepository     $taskRepository Task repository
-     * @param PaginatorInterface $paginator      Paginator
+     * @param Request $request HTTP Request
      *
      * @return Response HTTP response
      */
     #[Route(name: 'task_index', methods: 'GET')]
-    public function index(Request $request, TaskRepository $taskRepository, PaginatorInterface $paginator): Response
+    public function index(Request $request): Response
     {
-        $pagination = $paginator->paginate(
-            $taskRepository->queryAll(),
-            $request->query->getInt('page', 1),
-            TaskRepository::PAGINATOR_ITEMS_PER_PAGE
+        $pagination = $this->taskService->getPaginatedList(
+            $request->query->getInt('page', 1)
         );
 
         return $this->render('task/index.html.twig', ['pagination' => $pagination]);
     }
-// ...
 
     /**
      * Show action.
      *
-     * @param Task $task Task entity
+     * @param Task $task Task
      *
      * @return Response HTTP response
      */
@@ -53,13 +60,10 @@ class TaskController extends AbstractController
         '/{id}',
         name: 'task_show',
         requirements: ['id' => '[1-9]\d*'],
-        methods: 'GET',
+        methods: 'GET'
     )]
     public function show(Task $task): Response
     {
-        return $this->render(
-            'task/show.html.twig',
-            ['task' => $task]
-        );
+        return $this->render('task/show.html.twig', ['task' => $task]);
     }
 }

606739863c7602ade1e6e4b7a9de9d2cfd63f069
ovti <svdovti@gmail.com>
2023-05-10 09:47:18 +0200

    php

---
+++ app/src/Service/CategoryService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Category;
+use App\Repository\CategoryRepository;
+use Knp\Component\Pager\Pagination\PaginationInterface;
+use Knp\Component\Pager\PaginatorInterface;
+
+class CategoryService implements CategoryServiceInterface
+{
+    private CategoryRepository $categoryRepository;
+    private PaginatorInterface $paginator;
+
+    public function __construct(CategoryRepository $categoryRepository, PaginatorInterface $paginator)
+    {
+        $this->categoryRepository = $categoryRepository;
+        $this->paginator = $paginator;
+    }
+
+    public function save(Category $category): void
+    {
+        if (null == $category->getId()) {
+            $category->setCreatedAt(new \DateTimeImmutable());
+        }
+        $category->setUpdatedAt(new \DateTimeImmutable());
+
+        $this->categoryRepository->save($category);
+    }
+
+    public function getPaginatedList(int $page): PaginationInterface
+    {
+        // TODO: Implement getPaginatedList() method.
+    }
+}
